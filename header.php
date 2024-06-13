fba4cf74d8f5d3b4cebb7c769b0b993699b25a0b
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2024-06-13 17:05:12 +0200

    refactorized header

--- header.php
+++ header.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php
+language_attributes ();
+?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?=bloginfo ('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-    <?php wp_head(); ?>
+    <title><?=wp_title ('|', true, 'right');?></title>
+    <link rel="stylesheet" href="<?=get_stylesheet_uri ();?>">
+    <?=wp_head ();?>
 </head>
-<body <?php body_class(); ?>>
+<body <?=body_class ();?>>
     <header>
         <div class="container">
-            <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+        <!--
+            <h1><a href="<?=home_url ();?>"><?=bloginfo ('name');?></a></h1>
             <nav>
-                <?php
-                wp_nav_menu( array(
-                    'theme_location' => 'primary',
-                    'menu_class'     => 'nav-menu',
-                ) );
-                ?>
+                <?=wp_nav_menu (array ('theme_location' => 'primary', 'menu_class' => 'nav-menu'));?>
             </nav>
+            -->
         </div>
     </header>
     <main>
