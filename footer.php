e7a28c9cfbb1df727bbbc35b5189ed815003e58f
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2024-06-13 15:15:36 +0200

    Now the footer is what it should

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
     </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
+	<div class="contact-footer">
+		<span class="email"><a href="mailto:info@psicoadaia.com">info@psicoadaia.com</a></span>
+		<p class="copyright"><a href="/aviso-legal/">AVISO LEGAL</a></p>
+		<p>&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+	</div>
+	<?php wp_footer(); ?>
 </body>
 </html>
