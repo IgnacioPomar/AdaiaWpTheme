da102b63610639571028a58a5c58a21123e5f36e
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-01-28 11:21:34 +0100

    Supponrt image and themes in the posts

--- functions.php
+++ functions.php
@@ -3,3 +3,12 @@
 // Allow featured Image
 // https://stackoverflow.com/a/30235452/74785
 add_theme_support ('post-thumbnails');
+
+
+
+function add_custom_templates($templates) {
+    $templates['template-contact-form.php'] = 'Formulario de contacto';
+    $templates['template-team.php'] = 'Miembro del equipo';
+    return $templates;
+}
+add_filter('theme_page_templates', 'add_custom_templates');
