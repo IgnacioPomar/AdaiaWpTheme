4e255547209b9ad5819bd3d6b6e119393a35687b
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-01-29 12:26:04 +0100

    Formateado el footer

--- footer.php
+++ footer.php
@@ -1,9 +1,14 @@
     </main>
 	<div class="contact-footer">
-		<span class="email"><a href="mailto:info@psicoadaia.com">info@psicoadaia.com</a></span>
+		<p class="email"><a href="mailto:info@psicoadaia.com">info@psicoadaia.com</a></p>
 		<p class="copyright"><a href="/aviso-legal/">AVISO LEGAL</a></p>
-		<p>&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+		<p>&copy; <?php
+		echo date ("Y") . ' ' . bloginfo ('name');
+		?>. All rights reserved.</p>
 	</div>
-	<?php wp_footer(); ?>
+	<?php
+
+	wp_footer ();
+	?>
 </body>
 </html>
