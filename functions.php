cb9c528b94247fba1cc88e43d1408287cd967488
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-01-30 15:06:08 +0100

    Ya se soporta el menu en dispositivos grandes

--- functions.php
+++ functions.php
@@ -3,12 +3,36 @@
 // Allow featured Image
 // https://stackoverflow.com/a/30235452/74785
 add_theme_support ('post-thumbnails');
+add_theme_support ('title-tag');
 
 
-
-function add_custom_templates($templates) {
-    $templates['page-contact-form.php'] = 'Formulario de contacto';
-    $templates['template-team.php'] = 'Miembro del equipo';
-    return $templates;
+function AdaiaWpTheme_enqueue_styles ()
+{
+	wp_enqueue_style ('AdaiaWpTheme-style', get_stylesheet_uri (), array (), wp_get_theme ()->get ('Version'), 'all');
 }
-add_filter('theme_page_templates', 'add_custom_templates');
+add_action ('wp_enqueue_scripts', 'AdaiaWpTheme_enqueue_styles');
+
+
+function add_custom_templates ($templates)
+{
+	$templates ['page-contact-form.php'] = 'Formulario de contacto';
+	$templates ['template-team.php'] = 'Miembro del equipo';
+	return $templates;
+}
+add_filter ('theme_page_templates', 'add_custom_templates');
+
+
+function getMnuAnchored ()
+{
+	$baseUrl = home_url ();
+	$subpages = get_pages (array ('parent' => 0, 'sort_column' => 'menu_order'));
+	$retval = array ();
+	foreach ($subpages as $page)
+	{
+		// Skip the -1 pages: in this theme ar "independent pages"
+		if ($page->menu_order > 100 || $page->menu_order < 0) continue;
+
+		$retval [] = "<a href=\"$baseUrl/#{$page->post_name}\">" . esc_html ($page->post_title) . '</a>';
+	}
+	return $retval;
+}
--- header.php
+++ header.php
@@ -1,23 +1,42 @@
 <!DOCTYPE html>
-<html <?php
-language_attributes ();
-?>>
+<html <?=language_attributes ();?>>
 <head>
     <meta charset="<?=bloginfo ('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=wp_title ('|', true, 'right');?></title>
-    <link rel="stylesheet" href="<?=get_stylesheet_uri ();?>">
     <?=wp_head ();?>
 </head>
 <body <?=body_class ();?>>
+	<?=wp_body_open ();?>
     <header>
-        <div class="container">
-        <!--
-            <h1><a href="<?=home_url ();?>"><?=bloginfo ('name');?></a></h1>
+        <div id="mnuBg"><div id="mnuContainer">
+        	<label for="menu-toggle" class="mnuLbl">☰</label>
+            <input type="checkbox" id="menu-toggle" class="menu-toggle" />
+        	<div id="brand">
+        		<a href="<?=home_url ();?>"><span class="adaiaLogo"></span></a>
+        	</div>
             <nav>
-                <?=wp_nav_menu (array ('theme_location' => 'primary', 'menu_class' => 'nav-menu'));?>
+			<?php
+			$mnuOpcs = getMnuAnchored ();
+
+			$arrays = array_chunk ($mnuOpcs, 4);
+
+			$ulClass = '';
+			foreach ($arrays as $opcs)
+			{
+				$ulClass = ('' == $ulClass) ? 'leftMnu' : 'rightMnu';
+
+				echo '<ul class="' . $ulClass . '">';
+
+				foreach ($opcs as $opc)
+				{
+					echo "<li>$opc</li>";
+				}
+				echo '</ul>';
+			}
+
+			?>
             </nav>
-            -->
-        </div>
+           
+        </div></div>
     </header>
     <main>
