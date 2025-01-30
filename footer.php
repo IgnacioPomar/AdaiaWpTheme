1a1cb5a26072a681388321ec4276e8bf76812494
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-01-30 15:38:22 +0100

    FIX link

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     </main>
 	<div class="contact-footer">
 		<p class="email"><a href="mailto:info@psicoadaia.com">info@psicoadaia.com</a></p>
-		<p class="copyright"><a href="/aviso-legal/">AVISO LEGAL</a></p>
+		<p class="copyright"><a href="/legal/aviso-legal/">AVISO LEGAL</a></p>
 		<p>&copy; <?php
 		echo date ("Y") . ' ' . bloginfo ('name');
 		?>. All rights reserved.</p>
