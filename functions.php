c6147a86c898170498c5422df0d4cff41663ca72
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-04-02 14:34:08 +0200

    Podemos configurar la página de contacto

--- functions.php
+++ functions.php
@@ -112,3 +112,48 @@ function remove_gutenberg_styles_for_guests ()
 }
 add_action ('wp_enqueue_scripts', 'remove_gutenberg_styles_for_guests', 100);
 
+// ----------- Theme personalized fields ------------
+// Registrar el menú en Ajustes
+add_action ('admin_menu', function ()
+{
+	add_options_page ('Ajustes del tema Adaia', // Título de la página
+	'ThemeAdaiaSettings', // Nombre del menú
+	'manage_options', // Capacidad necesaria
+	'settings_adaia', // Slug del menú
+	'show_settings_adaia' // Función de callback
+	);
+});
+
+// Registrar el campo de configuración
+add_action ('admin_init', function ()
+{
+	register_setting ('settings_adaia', 'adaia_contact_page');
+
+	add_settings_section ('section_adaia_footer', 'Footer Configuration', null, 'settings_adaia');
+
+	add_settings_field ('adaia_contact_page', 'Página de contacto', function ()
+	{
+		// $valor = esc_attr (get_option ('adaia_contact_page', 'contacto'));
+		// echo "<input type='text' name='adaia_contact_page' value='$valor' />";
+		$selected = get_option ('adaia_contact_page');
+		wp_dropdown_pages ([ 'name' => 'adaia_contact_page', 'selected' => $selected, 'show_option_none' => '— Selecciona una página —', 'option_none_value' => '']);
+	}, 'settings_adaia', 'section_adaia_footer');
+});
+
+
+// Mostrar la página HTML de ajustes
+function show_settings_adaia ()
+{
+	?>
+    <div class="wrap">
+        <h1>Adaia Theme Settings</h1>
+        <form method="post" action="options.php">
+            <?php
+	settings_fields ('settings_adaia');
+	do_settings_sections ('settings_adaia');
+	submit_button ();
+	?>
+        </form>
+    </div>
+    <?php
+}
