8096fa784cf7f6fbb3ccc2fd4bec55d11a0f8dcd
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-04-02 14:44:33 +0200

    Show only when we are not in the same page

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
     </main>
 	<div class="contact-footer<?=is_front_page () ? ' frontpage' : ''?>">
-		<h2 id="contact"><a href="<?=get_permalink (get_option ('adaia_contact_page'));?>">¿Tienes una consulta? Contacta con nosotros</a></h2>
+	
+	<?php
+	$contact_page_id = get_option ('adaia_contact_page');
+	$current_page_id = get_queried_object_id ();
+
+	if ($current_page_id !== intval ($contact_page_id))
+	{
+		$url_contacto = get_permalink ($contact_page_id);
+		echo '<h2 id="contact"><a href="' . $url_contacto . '">¿Tienes una consulta? Contacta con nosotros</a></h2>';
+	}
+	?>
+	
 		<p class="email"><a href="mailto:info@psicoadaia.com">info@psicoadaia.com</a></p>
 		<p class="copyright"><a href="<?=home_url ();?>/legal/aviso-legal/">AVISO LEGAL</a></p>
 		<p>&copy; <?=date ("Y")?> Adaia&reg;. Todos los derechos reservados.</p>
